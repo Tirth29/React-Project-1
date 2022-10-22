4002ccbc61fa1d9dc9a7eaf06de692661f29ff0f
Tirth <tirthsolanki29923@gmail.com>
2022-10-22 13:40:18 +0530

    darkmodeadd

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import './navbar';
 import Navbar from './navbar';
 import TextForm from './Components/TextForm';
+import About from './Components/about';
 
 function App() {
   return (
@@ -11,10 +12,11 @@ function App() {
     <Navbar
      title="TextUtils1"
      aboutText="About textutils"/> 
-     {/* <Navbar/> */}
-    <div className="container-main">
+     <Navbar/>
+     <div className="container-main">
      <TextForm heading="Enter text to analyze"/>
      </div>
+     <About/>
     </>
   );
 }
---
+++ src/Components/about.js
@@ -0,0 +1,69 @@
+import React,{useState} from "react";
+export default function About() {
+
+  // let style={
+  //   color: 'white',
+  //   backgroundColor: 'black'
+  // }
+  const[style,setStyle]=useState({
+    color: 'white',
+    backgrounColor: 'black'
+  })
+  const[btnText,btnNewText]= useState("Enable Light Mode");
+
+  const toggleStyle= ()=>{
+
+    if(style.color === 'white')
+    {
+      setStyle(
+        {
+          color: 'black',
+          backgroundColor: 'white'
+        }
+      )
+      btnNewText("Enable Dark Mode");
+    }
+    else
+    {
+      setStyle(
+        {
+          color: 'white',
+          backgroundColor: 'black'
+        }
+      )
+      btnNewText("Enable Light Mode");
+    }
+
+  }
+
+  return (
+    <>
+    <div className="alert alert-primary" style={style} role="alert">
+    A simple primary alert—check it out!
+  </div>
+  <div className="alert alert-secondary" style={style} role="alert">
+    A simple secondary alert—check it out!
+  </div>
+  <div className="alert alert-success" style={style} role="alert">
+    A simple success alert—check it out!
+  </div>
+  <div className="alert alert-danger" style={style} role="alert">
+    A simple danger alert—check it out!
+  </div>
+  <div className="alert alert-warning" style={style} role="alert">
+    A simple warning alert—check it out!
+  </div>
+  <div className="alert alert-info" style={style} role="alert">
+    A simple info alert—check it out!
+  </div>
+  <div className="alert alert-light" style={style} role="alert">
+    A simple light alert—check it out!
+  </div>
+  <div className="alert alert-dark" style={style} role="alert">
+    A simple dark alert—check it out!
+  </div>
+  <button type="button" className="btn btn-primary" onClick={toggleStyle}>{btnText}</button>
+    </>
+   
+  )
+}
