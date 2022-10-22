212032f09845ffc5a69e86cad9bd73fb4e0abe28
Tirth <tirthsolanki29923@gmail.com>
2022-10-22 21:54:41 +0530

    darkthemedone

--- src/App.js
+++ src/App.js
@@ -4,13 +4,38 @@ import './navbar';
 import Navbar from './navbar';
 import TextForm from './Components/TextForm';
 import About from './Components/about';
+import React, {useState} from 'react'
+
 
 function App() {
+  const[mode,setMode]= useState('light');
+  const toggleMode = ()=>{
+    // if(mode==='dark')
+    // {
+    //   setMode('light');
+    // }
+    // else
+    // {
+    //     setMode('dark');
+    // }
+    if(mode==='light')
+    {
+      setMode('dark');
+      document.body.style.backgroundColor='#042743';
+
+    }
+    else
+    {
+      setMode('light');
+      document.body.style.backgroundColor='white';
+    
+    }
+  }
   return (
     <>
 
     <Navbar
-     title="TextUtils1"
+     title="TextUtils1" mode={mode} toggleMode={toggleMode}
      aboutText="About textutils"/> 
      {/* <Navbar/> */}
      <div className="container-main">
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -18,6 +18,10 @@ export default function TextForm(props) {
         setText(lowcase);
 
     }
+    const removeextraspace=()=>{
+        let newText=text.split(/[ ]+/);
+        setText(newText.join(" "));
+    }
     const copy=()=>{
         var text = document.getElementById("exampleFormControlTextarea1");
         text.select();
@@ -30,7 +34,7 @@ export default function TextForm(props) {
     const[text,setText] = useState("");
   return (
     <>
-    <div>
+    <div className='container' style={{color: props.mode==='dark'?'white':'#042743'}}>
         <h1>{props.heading}</h1>
       {/* <div className="mb-3">
   <label for="exampleFormControlInput1" className="form-label">Email address</label>
@@ -38,14 +42,15 @@ export default function TextForm(props) {
 </div> */}
 <div className="mb-3">
   <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-  <textarea className="form-control" value={text} onChange={handleonchange} id="exampleFormControlTextarea1" rows="3"></textarea>
+  <textarea className="form-control" value={text} onChange={handleonchange} id="exampleFormControlTextarea1" style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'#042743' }} rows="3"></textarea>
 </div>
 <button className='btn btn-primary' onClick={convertUpCase}>Convert to UpperCase</button><br></br><br></br>
 <button className='btn btn-primary' onClick={convertlowCase}>Convert to LowerCase</button><br></br><br></br>
 <button className='btn btn-primary' onClick={copy}>Copy Text</button><br></br><br></br>
-<button className='btn btn-primary' onClick={cleartext}>clearText</button>
+<button className='btn btn-primary' onClick={cleartext}>clearText</button> 
+<button className='btn btn-primary' onClick={removeextraspace}> Remove Extra Space</button>
     </div>
-    <div className='container'>
+    <div className='container my-32' style={{color: props.mode==='dark'?'white':'#042743'}}>
         <h1>Your Text Summary</h1>
         <p>{text.split(" ").length} words and {text.length} characters</p>
     <p>{0.008*text.split(" ").length} minutes read </p>
