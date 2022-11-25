6475a65beb5288c2707e5e8aa5df1ad2334c8ad9
Tirth <tirthsolanki29923@gmail.com>
2022-11-25 21:29:13 +0530

    problemsolve

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 // import logo from './logo.svg';
-import './App.css';
-import './navbar';
-import Navbar from './navbar';
-import TextForm from './Components/TextForm';
-import About from './Components/about';
-import React, {useState} from 'react'
+import "./App.css";
+import "./navbar";
+import Navbar from "./navbar";
+import TextForm from "./Components/TextForm";
+import About from "./Components/about";
+import React, { useState } from "react";
 // import Alert from "./Components/alert";
 
 import {
@@ -12,10 +12,10 @@ import {
   Routes,
   Route,
   // Link
-} from 'react-router-dom';
+} from "react-router-dom";
 
 function App() {
-  const[mode,setMode]= useState('light');
+  const [mode, setMode] = useState("light");
   // const[alert,setAlert]= useState('');
   // const showalert=(message,type)=>{
   //   setAlert(
@@ -25,7 +25,7 @@ function App() {
   //     }
   //   )
   // }
-  const toggleMode = ()=>{
+  const toggleMode = () => {
     // if(mode==='dark')
     // {
     //   setMode('light');
@@ -34,50 +34,46 @@ function App() {
     // {
     //     setMode('dark');
     // }
-    if(mode==='light')
-    {
-      setMode('dark');
-      document.body.style.backgroundColor='#042743';
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
       // showalert("Dark mode has been enabled","success");
       // document.title='TextUtils Dark Mode';
       // setInterval(() => {
       //     document.title = 'TextUtils is amazing'
       // }, 2000);
       // setInterval(() => {
-      //   document.title = 'Install TextUtils now !'; 
+      //   document.title = 'Install TextUtils now !';
       // }, 1500);
-
-
-    }
-    else
-    {
-      setMode('light');
-      document.body.style.backgroundColor='white';
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
       // showalert("Light mode has been enabled","success");
       // Document.title='TextUtils Light Mode';
     }
-  }
+  };
   return (
     <>
-
-    <Navbar
-     title="TextUtils1" mode={mode} toggleMode={toggleMode}
-     aboutText="About textutils"/> 
-     {/* <Navbar/> */}
-     {/* <Alert alert={alert}/> */}
-     <div className="container-main">
       <Router>
-        <Routes>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path='/'>
-     <TextForm heading="Enter text to analyze"/>
-          </Route>
-        </Routes>
+        <Navbar
+          title="TextUtils1"  
+          mode={mode}
+          toggleMode={toggleMode}
+          aboutText="About textutils"
+        />
+        {/* <Navbar/> */}
+        {/* <Alert alert={alert}/> */}
+        <div className="container-main">
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route
+              path="/"
+              element={<TextForm heading="Enter text to analyze" mode={mode} />}
+            />
+          </Routes>
+          <About/>
+        </div>
       </Router>
-     </div>
-     
     </>
   );
 }
--- src/Components/TextForm.js
+++ src/Components/TextForm.js
@@ -1,63 +1,99 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export default function TextForm(props) {
-    // Text=mytext;   wrong way to change
-    // setText=mytext;   write way to change
-    const convertUpCase=()=>
-    {
-        // console.log("Convert uppercase clicked"+ text);
-        let newText=text.toUpperCase();
-        setText(newText);
-    }
-    const handleonchange=(event)=>{
-        // console.log("On change");
-        setText(event.target.value);
-    }
-    const convertlowCase=()=>{
-        let lowcase=text.toLowerCase();
-        setText(lowcase);
-
-    }
-    const removeextraspace=()=>{
-        let newText=text.split(/[ ]+/);
-        setText(newText.join(" "));
-    }
-    const copy=()=>{
-        var text = document.getElementById("exampleFormControlTextarea1");
-        text.select();
-        navigator.clipboard.writeText(text.value)
-    }
-    const cleartext=()=>{
-        let newText='';
-        setText(newText);
-    }
-    const[text,setText] = useState('');
+  // Text=mytext;   wrong way to change
+  // setText=mytext;   write way to change
+  const convertUpCase = () => {
+    // console.log("Convert uppercase clicked"+ text);
+    let newText = text.toUpperCase();
+    setText(newText);
+  };
+  const handleonchange = (event) => {
+    // console.log("On change");
+    setText(event.target.value);
+  };
+  const convertlowCase = () => {
+    let lowcase = text.toLowerCase();
+    setText(lowcase);
+  };
+  const removeextraspace = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  };
+  const copy = () => {
+    var text = document.getElementById("exampleFormControlTextarea1");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  };
+  const cleartext = () => {
+    let newText = "";
+    setText(newText);
+  };
+  const [text, setText] = useState("");
   return (
     <>
-    <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}>
-        <h1 style={{color: props.mode==='dark'?'white':'#042743'}}>{props.heading}</h1>
-      {/* <div className="mb-3">
-  <label for="exampleFormControlInput1" className="form-label">Email address</label>
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
+        <h1 style={{ color: props.mode === "dark" ? "white" : "#042743" }}>
+          {props.heading}
+        </h1>
+        {/* <div className="mb-3">
+  <label htmlFor"exampleFormControlInput1" className="form-label">Email address</label>
   <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="name@example.com"/>
 </div> */}
-<div className="mb-3">
-  <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label>
-  <textarea className="form-control" value={text} onChange={handleonchange} id="exampleFormControlTextarea1" style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'#042743' }} rows="3"></textarea>
-</div>
-<button className='btn btn-primary' onClick={convertUpCase}>Convert to UpperCase</button><br></br><br></br>
-<button className='btn btn-primary' onClick={convertlowCase}>Convert to LowerCase</button><br></br><br></br>
-<button className='btn btn-primary' onClick={copy}>Copy Text</button><br></br><br></br>
-<button className='btn btn-primary' onClick={cleartext}>clearText</button> 
-<button className='btn btn-primary' onClick={removeextraspace}> Remove Extra Space</button>
-    </div>
-    <div className='container my-32' style={{color: props.mode==='dark'?'white':'#042743'}}>
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlTextarea1" className="form-label">
+            Example textarea
+          </label>
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleonchange}
+            id="exampleFormControlTextarea1"
+            style={{
+              backgroundColor: props.mode === "dark" ? "grey" : "white",
+              color: props.mode === "dark" ? "white" : "#042743",
+            }}
+            rows="3"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary" onClick={convertUpCase}>
+          Convert to UpperCase
+        </button>
+        <br></br>
+        <br></br>
+        <button className="btn btn-primary" onClick={convertlowCase}>
+          Convert to LowerCase
+        </button>
+        <br></br>
+        <br></br>
+        <button className="btn btn-primary" onClick={copy}>
+          Copy Text
+        </button>
+        <br></br>
+        <br></br>
+        <button className="btn btn-primary" onClick={cleartext}>
+          clearText
+        </button>
+        <button className="btn btn-primary" onClick={removeextraspace}>
+          {" "}
+          Remove Extra Space
+        </button>
+      </div>
+      <div
+        className="container my-32"
+        style={{ color: props.mode === "dark" ? "white" : "#042743" }}
+      >
         <h1>Your Text Summary</h1>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
-    <p>{0.008*text.split(" ").length} minutes read </p>
-    <h2>Preview</h2>
-    <p>{text}</p>
-
-    </div>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} minutes read </p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+      </div>
     </>
-  )
+  );
 }
